104093b95732fdea60c71d57a9277fa26502364b
Hemant2335 <knrt73373@gmail.com>
2023-09-04 18:50:10 +0530

    Poster_done_Lottie_added_assets_added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div className='h-[80px] w-full bg-[#343434] mt-[10vh] shadow-3xl flex justify-center items-center'>
+        <div>
+          <h1 className=''>You can help <br/> Shape the Future</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
