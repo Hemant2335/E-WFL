bab82c626b1107e51997f2caf7b33c5abe12b017
Hemant2335 <knrt73373@gmail.com>
2023-09-04 19:10:05 +0530

    Homepage_first_look

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/nLogo.png";
 
 const Footer = () => {
   return (
     <div>
-      <div className='h-[80px] w-full bg-[#343434] mt-[10vh] shadow-3xl flex justify-center items-center'>
+      <div className="h-fit w-full bg-[#222222] mt-[20vh] md:flex flex-col justify-center shadow-3xl  p-5">
         <div>
-          <h1 className=''>You can help <br/> Shape the Future</h1>
+          <h1 className="font-semibold font-montserrat text-xl text-[#FEFBE5] text-left">
+            You can help <br /> Shape the Future
+          </h1>
         </div>
+
+        <div className="mt-5 md:flex justify-between items-center">
+          <div className="flex gap-2 ">
+            <img src={Logo} alt="logo" className="h-[10vh]" />
+          </div>
+
+          <div>
+          <ul className="flex gap-2">
+            <li
+              className="font-semibold font-montserrat  hover:text-[#ff5757] cursor-pointer"
+              onClick={() => navigate("/")}
+            >
+              <a>Home |</a>
+            </li>
+            <li
+              className="font-semibold font-montserrat  hover:text-[#ff5757] cursor-pointer"
+              onClick={()=>document.getElementById("about").scrollIntoView({behavior:"smooth"})}
+            >
+              <a>About |</a>
+            </li>
+            <li
+              className="font-semibold font-montserrat hover:text-[#ff5757] cursor-pointer"
+              onClick={()=>document.getElementById("contact").scrollIntoView({behavior:"smooth"})}
+            >
+              <a>Education |</a>
+            </li>
+            <li
+              className="font-semibold font-montserrat hover:text-[#ff5757] cursor-pointer"
+              onClick={()=>document.getElementById("contact").scrollIntoView({behavior:"smooth"})}
+            >
+              <a>Contact</a>
+            </li>
+          </ul>
+          </div>
+        </div>
+        <div className="flex  items-center justify-center mt-5">
+          <h1 className="flex justify-center text-xl text-left   font-montserrat font-semibold gap-2">
+            Created by<span className="font-bold text-xl  font-montserrat text-[#ff5757]">EWFL</span>team
+          </h1>
+          </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
