8ef2dc6d4df5bb546436750a2a28681d93614293
Hemant2335 <knrt73373@gmail.com>
2023-09-14 21:04:50 +0530

    Check_update_done

--- src/Components/Poster.jsx
+++ src/Components/Poster.jsx
@@ -98,7 +98,7 @@ const Poster = () => {
     <Wrapper>
       {isLoading ? <Loading /> : ""}
       <div
-        className="flex relative rounded-lg  mt-[5vh] gap-10 justify-center items-center md:max-h-[70vh] "
+        className="flex relative rounded-lg  pt-[2vh] mt-[5vh] gap-10 justify-center items-center md:max-h-[70vh] "
         id="searchposter"
       >
         <div className="hidden absolute opacity-10 top-0 md:flex w-full max-h-[75vh] ">
@@ -108,7 +108,7 @@ const Poster = () => {
             className="max-h-[100vh] w-full object-cover rounded-xl"
           />
         </div>
-        <div className="w-full h-fit  mt-[5vh] shadow-3xl rounded-xl p-[5vh] other md:ml-5 mb-10 z-10 searchtext ">
+        <div className="w-full h-fit  mt-[5vh] shadow-3xl rounded-xl p-[2.5vw]  other md:ml-5 mb-10 z-10 searchtext ">
           <h1 className="md:text-[5vh] text-[5vh] font-montserrat  font-bold">
             Search E-waste facilities
           </h1>
